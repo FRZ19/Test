5b5608ad1db7162873209ebf0f41444089c7e8f9
FRZ19 <160596298+FRZ19@users.noreply.github.com>
2024-03-06 13:30:00 +0700

    Add files via upload

--- lib/page/edit_page.dart
+++ lib/page/edit_page.dart
@@ -20,90 +20,153 @@ class _NoteDetailPageState extends State<NoteDetailPage> {
   @override
   void initState() {
     super.initState();
-    _titleController = TextEditingController(
-      text: widget.nota.title,
-    );
-    _contentController = TextEditingController(
-      text: widget.nota.content,
-    );
+    _titleController = TextEditingController(text: widget.nota.title,);
+    _contentController = TextEditingController(text: widget.nota.content,);
 
-    //inisialisasi nilai checklist item sesuai dengan jumlah item yang sudah ada
+   // inisialisasi  nilai checklistitems hanya jika konten nota tidak kosong
+    if (widget.nota.content.isNotEmpty) {
+      _initializeChecklistItems();
+    } else {
+      _checklistItems = [];
+    }
+  }
+
+  // fungsi  untuk inisialisasi checklistitems
+  void _initializeChecklistItems() {
     _checklistItems = List<bool>.generate(
-      widget.nota.content.split('\n').length,
+      _contentController.text.isNotEmpty ? _contentController.text.split('\n').length : 0,
         (index) => false,
     );
-
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Edit Nota'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.save),
-            onPressed: () {
-              widget.nota.title = _titleController.text;
-              widget.nota.content = _titleController.text + '\n' + _contentController.text;
-              Navigator.of(context).pop(widget.nota);
-            },
-          ),
-        ],
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextField(
-              controller: _titleController,
-              decoration: InputDecoration(labelText: 'Judul Nota'),
+    return WillPopScope(
+      onWillPop: () async {
+        return true;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Edit Nota'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: _deleteNote,
             ),
-            SizedBox(height: 16.0,),
-            Expanded(
-              child: ListView.builder(
-                itemCount: _checklistItems.length,
-                itemBuilder: (context, index) {
-                  return CheckboxListTile(
-                    title: Text(
-                      widget.nota.content.split('\n')[index],
-                      style: TextStyle(
-                        decoration: _checklistItems[index]
-                            ? TextDecoration.lineThrough
-                            : TextDecoration.none,
-                      ),
-                    ),
-                    value: _checklistItems[index],
-                    onChanged: (newValue) {
-                      setState(() {
-                        _checklistItems[index] = newValue!;
-                      });
-                    },
-                  );
-              },
-              ),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              controller: _addItemController,
-              decoration: InputDecoration(labelText: 'Tambah Item'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  _checklistItems.add(false);
-                  widget.nota.content += '\n' + _addItemController.text;
-                  _addItemController.clear();
-                });
-              },
-              child: Text('Tambah item'),
+            IconButton(
+              icon: Icon(Icons.save),
+              onPressed: _saveNote,
             ),
           ],
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                controller: _titleController,
+                decoration: InputDecoration(labelText: 'Judul Nota'),
+              ),
+              SizedBox(height: 16.0,),
+              if (_checklistItems.isNotEmpty)
+              Expanded(
+                child: ListView.builder(
+                  itemCount: _checklistItems.length,
+                  itemBuilder: (context, index) {
+                    return CheckboxListTile(
+                      title: Text(
+                        widget.nota.content.split('\n')[index],
+                        style: TextStyle(
+                          decoration: _checklistItems[index]
+                              ? TextDecoration.lineThrough
+                              : TextDecoration.none,
+                        ),
+                      ),
+                      value: _checklistItems[index],
+                      onChanged: (newValue) {
+                        setState(() {
+                          _checklistItems[index] = newValue!;
+                        });
+                      },
+                    );
+                },
+                ),
+              ),
+              SizedBox(height: 16.0),
+              TextField(
+                controller: _addItemController,
+                decoration: InputDecoration(labelText: 'Tambah Item'),
+              ),
+              ElevatedButton(
+                onPressed: _addItem,
+                child: Text('Tambah item'),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+
+  void _addItem() {
+    setState(() {
+      final newItem = _addItemController.text.trim();
+      if (newItem.isNotEmpty) {
+        _checklistItems.add(false);
+        widget.nota.content += '\n' + newItem;
+        _addItemController.clear();
+      }
+    });
+  }
+
+  void _saveNote() {
+    setState(() {
+      //simpan judul dan konten ke dalam objek nota
+      widget.nota.title = _titleController.text;
+      widget.nota.content = _contentController.text;
+
+      // simpan status chekbox ke dalam konten nota
+      List<String> lines = widget.nota.content.split('\n');
+      for (int i = 0; i < _checklistItems.length && i < lines.length; i++) {
+        if (_checklistItems[i]) {
+          lines[i] = '[x]' + lines[i].substring(4);
+        } else {
+          lines[i] = '[ ]' + lines[i].substring(4);
+        }
+      }
+      widget.nota.content = lines.join('\n');
+    });
+    Navigator.of(context).pop(widget.nota);
+  }
+
+  void _deleteNote() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Hapus nota'),
+          content: Text('Apakah anda yakin ingin menghapus nota?'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Tidak'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Ya'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      }
+    );
+  }
+
 }
 
 class Nota {
